f52c0378961eb112007c2e0e3fa245a2bf3eddab
Thanh Phong <phongpronood@gmail.com>
2025-11-02 13:17:45 +0700

    đkí và đnhập

---
+++ src/main/java/uth/edu/vn/ccmarket/controller/AuthController.java
@@ -0,0 +1,37 @@
+package uth.edu.vn.ccmarket.controller;
+
+import uth.edu.vn.ccmarket.model.EVOwner;
+import uth.edu.vn.ccmarket.repository.EVOwnerRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AuthController {
+    private final EVOwnerRepository repo;
+    private final BCryptPasswordEncoder encoder;
+
+    public AuthController(EVOwnerRepository repo, BCryptPasswordEncoder encoder) {
+        this.repo = repo;
+        this.encoder = encoder;
+    }
+
+    @GetMapping("/register")
+    public String registerForm(Model m) {
+        m.addAttribute("user", new EVOwner());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String doRegister(@ModelAttribute EVOwner user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        repo.save(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
