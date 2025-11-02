a53c2d77079aca7524a771b8a8712ee4bab4f608
Thanh Phong <phongpronood@gmail.com>
2025-11-03 02:58:48 +0700

    tín chỉ carbon

--- src/main/java/uth/edu/vn/ccmarket/model/CarbonCredit.java
+++ src/main/java/uth/edu/vn/ccmarket/model/CarbonCredit.java
@@ -11,5 +11,57 @@ public class CarbonCredit {
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY) // nhiều tín chỉ - 1 chủ xe
     @JoinColumn(name = "owner_id", nullable = false)
-    private EVOwner owner;
+    private EVOwner owner;// chủ sở hữu tín chỉ
+    private double quantity; // số lượng tín chỉ (kg CO2)
+    private LocalDate issuedAt;
+    private boolean verified = false;// xác minh hay chưa
+
+    public CarbonCredit() {
+    }
+
+    public CarbonCredit(EVOwner owner, double quantity, LocalDate issuedAt) {
+        this.owner = owner;
+        this.quantity = quantity;
+        this.issuedAt = issuedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public EVOwner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(EVOwner owner) {
+        this.owner = owner;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getIssuedAt() {
+        return issuedAt;
+    }
+
+    public void setIssuedAt(LocalDate issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
 }
