a84a13e1005e397a22ba472acd9d337eefcb57e4
Thanh Phong <phongpronood@gmail.com>
2025-11-02 12:34:48 +0700

    add CcmarketWebApplication.java

---
+++ src/main/java/uth/edu/vn/ccmarket/CcmarketWebApplication.java
@@ -0,0 +1,11 @@
+package uth.edu.vn.ccmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcmarketWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CcmarketWebApplication.class, args);
+    }
+}
