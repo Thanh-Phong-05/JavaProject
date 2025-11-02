b4614fd4cb41db4687d3d35d1139507b7612ecdc
Thanh Phong <phongpronood@gmail.com>
2025-11-03 01:19:44 +0700

    tên đăng nhập, pass, email, tiền quy đổi được, tín chỉ đổi từ CO2

--- src/main/java/uth/edu/vn/ccmarket/model/EVOwner.java
+++ src/main/java/uth/edu/vn/ccmarket/model/EVOwner.java
@@ -1,5 +1,8 @@
 package uth.edu.vn.ccmarket.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -8,74 +11,11 @@ public class EVOwner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(unique = true)
     private String username;
     private String password;
     private String email;
 
-    private double cashBalance = 0.0;
-    private double creditBalance = 0.0;
-
-    public EVOwner() {
-    }
-
-    public EVOwner(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    // getters & setters
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String u) {
-        this.username = u;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String p) {
-        this.password = p;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String e) {
-        this.email = e;
-    }
-
-    public double getCashBalance() {
-        return cashBalance;
-    }
-
-    public void depositCash(double v) {
-        this.cashBalance += v;
-    }
-
-    public double getCreditBalance() {
-        return creditBalance;
-    }
-
-    public void depositCredits(double q) {
-        this.creditBalance += q;
-    }
-
-    public boolean withdrawCredits(double q) {
-        if (q <= creditBalance) {
-            creditBalance -= q;
-            return true;
-        }
-        return false;
-    }
+    double cashBalance = 0.0; // tiền mặt
+    double creditBalance = 0.0; // tín chỉ
 }
