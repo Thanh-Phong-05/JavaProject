f674896b80900000b17d25f98aaf3ad1afe6550e
Thanh Phong <phongpronood@gmail.com>
2025-11-02 13:00:03 +0700

    ListingRepository.java

---
+++ src/main/java/uth/edu/vn/ccmarket/repository/ListingRepository.java
@@ -0,0 +1,9 @@
+package uth.edu.vn.ccmarket.repository;
+
+import uth.edu.vn.ccmarket.model.Listing;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ListingRepository extends JpaRepository<Listing, Long> {
+    List<Listing> findByActiveTrue();
+}
