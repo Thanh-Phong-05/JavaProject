46950c3c7f780c8bd05bf5b8ffdf20bc2e50a013
Thanh Phong <phongpronood@gmail.com>
2025-11-03 03:11:18 +0700

    Quản lý tín chỉ mỗi chủ xe

--- src/main/java/uth/edu/vn/ccmarket/repository/CarbonCreditRepository.java
+++ src/main/java/uth/edu/vn/ccmarket/repository/CarbonCreditRepository.java
@@ -1,7 +1,8 @@
 package uth.edu.vn.ccmarket.repository;
 
-import uth.edu.vn.ccmarket.model.CarbonCredit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import uth.edu.vn.ccmarket.model.CarbonCredit;
+
 import java.util.List;
 
 public interface CarbonCreditRepository extends JpaRepository<CarbonCredit, Long> {
