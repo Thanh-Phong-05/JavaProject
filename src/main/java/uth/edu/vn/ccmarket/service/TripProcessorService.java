e06a3fe2db5c6b2e490fa94fb1618f1cc74d5dbe
Thanh Phong <phongpronood@gmail.com>
2025-11-03 01:38:29 +0700

    giả sử 1km thì tiết kiệm 0.12

--- src/main/java/uth/edu/vn/ccmarket/service/TripProcessorService.java
+++ src/main/java/uth/edu/vn/ccmarket/service/TripProcessorService.java
@@ -1,21 +1,22 @@
 package uth.edu.vn.ccmarket.service;
 
-import uth.edu.vn.ccmarket.model.CarbonCredit;
-import uth.edu.vn.ccmarket.model.EVOwner;
+import uth.edu.vn.ccmarket.model.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TripProcessorService {
-    private static final double ICE_KG_PER_KM = 0.25;
-    private static final double EV_KG_PER_KM = 0.05;
+    private static final double CO2_CHANGE = 0.120;
 
-    public CarbonCredit createCreditFromKm(EVOwner owner, double totalKm) {
-        double totalReductionKg = totalKm * (ICE_KG_PER_KM - EV_KG_PER_KM);
-        double tonnes = totalReductionKg / 1000.0;
-        if (tonnes <= 0)
-            throw new IllegalArgumentException("No reduction");
-        CarbonCredit cc = new CarbonCredit(owner.getId(), tonnes);
-        owner.depositCredits(tonnes);
-        return cc;
+    public double calculateCO2Saved(double distanceKm) {
+        return distanceKm * CO2_CHANGE;
     }
+
 }
