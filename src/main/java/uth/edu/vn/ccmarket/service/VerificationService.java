da5471190ac28a0f5bf91d82089d007f73c895b1
Thanh Phong <phongpronood@gmail.com>
2025-11-02 13:07:32 +0700

    Xác minh số lượng cocbon real hay fake

--- src/main/java/uth/edu/vn/ccmarket/service/VerificationService.java
+++ src/main/java/uth/edu/vn/ccmarket/service/VerificationService.java
@@ -1,22 +1,15 @@
 package uth.edu.vn.ccmarket.service;
 
-import uth.edu.vn.ccmarket.model.CVA;
+import org.springframework.stereotype.Service;
 import uth.edu.vn.ccmarket.model.CarbonCredit;
 
-/**
- * CVA checks (very simple): if quantity > 0 and owner exists => verify
- */
+@Service
 public class VerificationService {
-
-    public boolean verifyCredit(CVA cva, CarbonCredit credit) {
-        // In real life, would check telemetry, raw data, signatures...
-        if (credit.getQuantity() > 0) {
-            credit.setVerified(true);
-            System.out.println("CVA " + cva.getName() + " approved credit " + credit.getCreditId());
+    public boolean verify(CarbonCredit cc) {
+        if (cc.getQuantity() > 0) {
+            cc.setVerified(true);
             return true;
-        } else {
-            System.out.println("CVA " + cva.getName() + " rejected credit " + credit.getCreditId());
-            return false;
         }
+        return false;
     }
 }
