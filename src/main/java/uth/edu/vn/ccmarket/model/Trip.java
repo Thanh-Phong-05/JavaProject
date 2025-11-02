3f8daaedc0e0a19ba1f12c8feaa67d6529a705b0
Thanh Phong <phongpronood@gmail.com>
2025-11-03 01:30:09 +0700

    Lưu hành trình(số km) cho chủ

--- src/main/java/uth/edu/vn/ccmarket/model/Trip.java
+++ src/main/java/uth/edu/vn/ccmarket/model/Trip.java
@@ -5,13 +5,19 @@ import java.time.LocalDate;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 public class Trip {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate date;
-    private double distanceKm; // in kilometers
-    private double energyConsumedKWh; // in kWh
+    private double distanceKm;
+    private double energyConsumedKWh;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private EVOwner owner;
 
 }
