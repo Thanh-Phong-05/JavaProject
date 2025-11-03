d63d13dcc0950b5e5bf32b7ccfa22807ec534f2d
Thanh Phong <phongpronood@gmail.com>
2025-11-04 01:56:50 +0700

    Lấy db bài rao bán

--- src/main/java/uth/edu/vn/ccmarket/repository/ListingRepository.java
+++ src/main/java/uth/edu/vn/ccmarket/repository/ListingRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ListingRepository extends JpaRepository<Listing, Long> {
-    List<Listing> findByActiveTrue();
+    List<Listing> findByActive(boolean active);
 }
