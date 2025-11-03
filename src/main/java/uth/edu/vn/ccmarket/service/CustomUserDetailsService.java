987befe18a68b4d60ead95fa6886bd0b579a0b86
Thanh Phong <phongpronood@gmail.com>
2025-11-04 00:33:12 +0700

    Truy xuất dữ liệu của chủ xe trong data và quyền hạn của chủ xe

--- src/main/java/uth/edu/vn/ccmarket/service/CustomUserDetailsService.java
+++ src/main/java/uth/edu/vn/ccmarket/service/CustomUserDetailsService.java
@@ -2,20 +2,34 @@ package uth.edu.vn.ccmarket.service;
 
 import uth.edu.vn.ccmarket.model.EVOwner;
 import uth.edu.vn.ccmarket.repository.EVOwnerRepository;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    private final EVOwnerRepository repo;
 
-    public CustomUserDetailsService(EVOwnerRepository repo) {
-        this.repo = repo;
+    private final EVOwnerRepository evOwnerRepository;
+
+    public CustomUserDetailsService(EVOwnerRepository evOwnerRepository) {
+        this.evOwnerRepository = evOwnerRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        EVOwner u = repo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return User.withUsername(u.getUsername()).password(u.getPassword()).roles("USER").build();
+
+        EVOwner owner = evOwnerRepository.findByUsername(username);
+
+        if (owner == null) {
+            throw new UsernameNotFoundException("Không tìm thấy người dùng: " + username);
+        }
+        return new User(
+                owner.getUsername(),
+                owner.getPassword(),
+                new ArrayList<>());
     }
 }
