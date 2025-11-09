600c562ac07a0342e052a84aebd6883fb28f3a50
Thanh Phong <phongpronood@gmail.com>
2025-11-10 02:28:14 +0700

    thêm repo ng mua để check

--- src/main/java/uth/edu/vn/ccmarket/service/CustomUserDetailsService.java
+++ src/main/java/uth/edu/vn/ccmarket/service/CustomUserDetailsService.java
@@ -1,33 +1,56 @@
 package uth.edu.vn.ccmarket.service;
 
+import uth.edu.vn.ccmarket.model.CCBuyer;
 import uth.edu.vn.ccmarket.model.EVOwner;
+import uth.edu.vn.ccmarket.repository.CCBuyerRepository;
 import uth.edu.vn.ccmarket.repository.EVOwnerRepository;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final EVOwnerRepository evOwnerRepository;
+    private final CCBuyerRepository buyerRepository; // check ng mua
 
-    public CustomUserDetailsService(EVOwnerRepository evOwnerRepository) {
+    public CustomUserDetailsService(EVOwnerRepository evOwnerRepository, CCBuyerRepository buyerRepository) {
         this.evOwnerRepository = evOwnerRepository;
+        this.buyerRepository = buyerRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        EVOwner owner = evOwnerRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        // check chu xe
+        Optional<EVOwner> ownerOpt = evOwnerRepository.findByUsername(username);
 
-        return new User(
-                owner.getUsername(),
-                owner.getPassword(),
-                new ArrayList<>());
+        if (ownerOpt.isPresent()) {
+            EVOwner owner = ownerOpt.get();
+            return new User(
+                    owner.getUsername(),
+                    owner.getPassword(),
+                    Collections.singletonList(new SimpleGrantedAuthority("ROLE_EV_OWNER")));
+        }
+
+        // check sang nguoi mua
+        Optional<CCBuyer> buyerOpt = buyerRepository.findByUsername(username);
+
+        if (buyerOpt.isPresent()) {
+            CCBuyer buyer = buyerOpt.get();
+            return new User(
+                    buyer.getUsername(),
+                    buyer.getPassword(),
+                    Collections.singletonList(new SimpleGrantedAuthority("ROLE_CC_BUYER")));
+        }
+
+        throw new UsernameNotFoundException("Không tìm thấy người dùng (trong cả EVOwner và CCBuyer): " + username);
     }
 }
