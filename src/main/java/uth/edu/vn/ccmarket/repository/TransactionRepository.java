79d9ce8db482c9b704ae58fef8cc2c2cc3045f78
huytpd1501 <huytran15012005@gmail.com>
2025-11-11 12:44:55 +0700

    sửa method

--- src/main/java/uth/edu/vn/ccmarket/repository/TransactionRepository.java
+++ src/main/java/uth/edu/vn/ccmarket/repository/TransactionRepository.java
@@ -1,13 +1,12 @@
 package uth.edu.vn.ccmarket.repository;
 
-import uth.edu.vn.ccmarket.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// THÊM:
-import java.util.List;
-import org.springframework.data.domain.Pageable;
+import uth.edu.vn.ccmarket.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    // THÊM: lấy các giao dịch hoàn tất mới nhất
-    List<Transaction> findByStatusOrderByIdDesc(Transaction.Status status, Pageable pageable);
+    // Dùng findAll(Pageable) để lấy bản ghi mới nhất trong service (sort theo id desc)
+    Page<Transaction> findAll(Pageable pageable);
 }
