26e60251d6da682fec0f84e03f9b5b5d0bcb1755
Thanh Phong <phongpronood@gmail.com>
2025-11-18 01:53:31 +0700

    repo ad

---
+++ src/main/java/uth/edu/vn/ccmarket/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package uth.edu.vn.ccmarket.repository;
+
+import uth.edu.vn.ccmarket.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByUsername(String username);
+}
