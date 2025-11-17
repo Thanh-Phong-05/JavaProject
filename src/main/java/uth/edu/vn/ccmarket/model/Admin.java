ca267a76cf97548b93ced5c9dc81b0f607dd67b3
Thanh Phong <phongpronood@gmail.com>
2025-11-18 01:51:48 +0700

    Tạo admin

---
+++ src/main/java/uth/edu/vn/ccmarket/model/Admin.java
@@ -0,0 +1,71 @@
+package uth.edu.vn.ccmarket.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    private String name;
+    private String authority; // Quyền hạn
+    private String license; // Giấy phép
+
+    public Admin() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+}
