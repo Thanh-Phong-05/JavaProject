7bcdaf300719057c6099724acaf651d69d8868df
Thanh Phong <phongpronood@gmail.com>
2025-11-18 02:26:12 +0700

    lịch sử ng mua

--- src/main/java/uth/edu/vn/ccmarket/repository/TransactionRepository.java
+++ src/main/java/uth/edu/vn/ccmarket/repository/TransactionRepository.java
@@ -3,10 +3,12 @@ package uth.edu.vn.ccmarket.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import uth.edu.vn.ccmarket.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    // Dùng findAll(Pageable) để lấy bản ghi mới nhất trong service (sort theo id desc)
     Page<Transaction> findAll(Pageable pageable);
+
+    // tìm giao dịch theo ng mua
+    List<Transaction> findBySellerId(Long sellerId);
 }
